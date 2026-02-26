4e2bb523b5eecd19900bdf485a5fdefa9a0f8b7e
covstar <okechukwuvictor093@gmail.com>
2026-02-26 08:51:46 -0800

    feat: set up Next.js `_document` and `_app` to include Google Tag Manager, Google Fonts, and Google Maps API.

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,19 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="icon" type="image/png" href="/RSS-logo.png" />
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-K56RHDB3');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
       </Head>
       <body className="antialiased">
         {/* Google Tag Manager (noscript) */}
