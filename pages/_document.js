56fcb11497236a941004499c284045640a95e55b
covstar <okechukwuvictor093@gmail.com>
2026-02-26 08:30:03 -0800

    feat: Setted up Next.js custom `_document` and `_app` for global layout, fonts, favicon, GTM, and Google Maps.

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,16 @@ export default function Document() {
         <link rel="icon" type="image/png" href="/RSS-logo.png" />
       </Head>
       <body className="antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K56RHDB3"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Main />
         <NextScript />
       </body>
